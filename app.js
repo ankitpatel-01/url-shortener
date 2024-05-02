5ec824592e592c28761375c8856b5059c71d0ef0
“ankitpatel” <ankitpatelak2017@gmail.com>
2024-05-02 22:12:02 +0530

    fix: cros

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ const app = express()
 db();
 cloudinary();
 
+// Allow all origins to access your API (not recommended for production)
+app.use(cors());
+
+// Or specify specific origins that are allowed to access your API
+// Replace 'http://example.com' with the origin(s) you want to allow
+// Allow only requests from 'http://example.com' and 'http://localhost:8080'
+// app.use(cors({ 
+//     origin: ['http://example.com', 'http://localhost:8080'] 
+//   }));
+
+
+
 app.use(express.json({
     extended: false
 }));
